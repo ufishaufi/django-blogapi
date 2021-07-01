582d8c585b607756f0bb66028bd98934e9c86b49
Shaufi <shaufiman@gmail.com>
2021-07-01 12:38:10 +0800

    upload file blog api for drf

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # 3rd party apps
+    'rest_framework',
+
     # local
     'posts.apps.PostsConfig',
 ]
@@ -121,6 +124,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
