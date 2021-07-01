17ae6be64ba57349574b71042b57b0c9e8ceca12
Shaufi <shaufiman@gmail.com>
2021-07-02 07:42:53 +0800

    upload file blog api for Default Authentication

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -129,7 +129,12 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
 
-    ]
+    ],
+
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ],
 }
 
 # Default primary key field type
